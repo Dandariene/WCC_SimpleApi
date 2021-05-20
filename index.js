667658cb7e5bbf5451aeffc3235c9dd6af81e2cd
Dandariene <dandariene@hotmail.com>
2021-05-20 18:54:49 -0300

    Iniciando criação de API

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const configExpress = require('./config/configExpress');
+const conexao = require('./infra/conexao');
+const Tabelas = require('./infra/Tabelas');
 
-app = configExpress();
+conexao.connect(error =>{
+    if(error) {
+        throw error;
+    };
+    Tabelas.init(conexao);
+    
+    app = configExpress();
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+    app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+});
+
+
+
 
